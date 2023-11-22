beb10b5181a4e410898c356c309bf87f17cdaf93
Egor Devyatkin <inkey.cherry@gmail.com>
2023-11-23 00:17:16 +0300

    fix - 2

--- main.py
+++ main.py
@@ -1,49 +1,134 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-from Function import Methods
+def fitness_function(x, y):
+    return np.sin(x) * np.cos(y) / (1 + x**2 + y**2)
 
-population_size = 4
-num_generations = 100
-mutation_rate = 0.25
-min_x = 0.0
-max_x = 2.0
-min_y = -2.0
-max_y = 2.0
+def initialize_population(population_size):
+    population = []
+    for _ in range(population_size):
+        x = np.random.uniform(0, 2)
+        y = np.random.uniform(-2, 2)
+        population.append((x, y))
+    return population
 
-population = Methods.initialize_population(population_size, min_x, max_x, min_y, max_y)
-initial_mutation_rate = mutation_rate
+def roulette_selection(population, fitness_values):
+    total_fitness = sum(fitness_values)
+    probabilities = [max(0, fitness / total_fitness) for fitness in fitness_values]
+    probabilities /= np.sum(probabilities)
+    selected_index = np.random.choice(len(population), p=probabilities)
+    return population[selected_index]
 
-generation_list = []
-average_fitness_list = []
+def ordered_crossover(parent1, parent2):
+    alpha = np.random.rand()
+    x_child = alpha * parent1[0] + (1 - alpha) * parent2[0]
+    y_child = alpha * parent1[1] + (1 - alpha) * parent2[1]
+    return (x_child, y_child)
 
-for generation in range(num_generations):
-    population = Methods.assess_population(population)
-    parents = Methods.choose_parents(population)
+def mutation(child, mutation_rate):
+    if np.random.rand() < mutation_rate:
+        gene_to_mutate = np.random.choice([0, 1])
+        if gene_to_mutate == 0:
+            child = (child[0] + np.random.uniform(-0.1, 0.1), child[1])
+        else:
+            child = (child[0], child[1] + np.random.uniform(-0.1, 0.1))
+    return child
 
-    Methods.display_results(population, generation)
-    current_convergence = generation / num_generations
-    mutation_rate = initial_mutation_rate * (1.0 - current_convergence)
+def plot_3d_surface():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-    for j in range(0, population_size - 1, 2):
-        child1 = Methods.perform_crossover(parents[j], parents[j + 1])
-        child1 = Methods.apply_mutation(child1, mutation_rate, min_x, max_x, min_y, max_y)
+    x_vals = np.linspace(0, 2, 100)
+    y_vals = np.linspace(-2, 2, 100)
+    X, Y = np.meshgrid(x_vals, y_vals)
+    Z = fitness_function(X, Y)
 
-        population[j] = child1
-        population[j].x = child1.x
-        population[j].y = child1.y
-        population[j].fitness = Methods.calculate_fitness(population[j].x, population[j].y)
+    ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Fitness')
 
-    if Methods.check_population_convergence(population):
-        print("Fitness Convergence", generation)
-        break
+    plt.savefig('3D.png')
 
-    generation_list.append(generation)
-    average_fitness = sum(p.fitness for p in population) / len(population)
-    average_fitness_list.append(average_fitness)
 
-Methods.draw_fitness_graph(generation_list[:10], average_fitness_list[:10])
-Methods.draw_scatter_plots(population)
+def plot_average_fitness(generation_values, average_fitness_values):
+    plt.figure()
+    plt.plot(generation_values[:len(average_fitness_values)], average_fitness_values, marker='o',
+             label='Average Fitness')
+    plt.title('Average Fitness for Generations')
+    plt.xlabel('Generation')
+    plt.ylabel('Average Fitness')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig('average - fitness.png')
 
-best_individual = max(population, key=lambda x: x.fitness)
-print("Оптимальное решение: \nx =", round(best_individual.x, 4), ", y =", round(best_individual.y, 4), " \nf(x, y) =", round(best_individual.fitness, 4))
+def plot_population_values(generation, x_values, y_values):
+    plt.figure()
+    plt.scatter(x_values, y_values, marker='o', label='Population Values')
 
+    additional_points_x = [0, 2]
+    additional_points_y = [-2, 2]
+    plt.scatter(additional_points_x, additional_points_y, color='red', marker='o', label='Critical Points')
+
+    plt.title(f'Population Values for Generation {generation}')
+    plt.xlabel('X values')
+    plt.ylabel('Y values')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'population - values - {generation}.png')
+
+def genetic_algorithm(population_size, generations, populations_to_display):
+    population = initialize_population(population_size)
+    best_overall_fitness = -np.inf
+    best_overall_individual = None
+    average_fitness_values = []
+
+    for generation in range(generations):
+        fitness_values = [fitness_function(x, y) for x, y in population]
+
+        if generation in populations_to_display:
+            print(f"\nX values for Population {generation}:")
+            for i in range(min(10, population_size)):
+                print(round(population[i][0], 4))
+
+            print(f"\nY values for Population {generation}:")
+            for i in range(min(10, population_size)):
+                print(round(population[i][1], 4))
+
+            print(f"\nFitness values for Population {generation}:")
+            for i in range(min(10, population_size)):
+                print(round(fitness_values[i], 4))
+
+            average_fitness = np.mean(fitness_values)
+            print(f"\nAverage Fitness for Population {generation}: {round(average_fitness, 4)}")
+            print(f"Max Fitness for Population {generation}: {round(max(fitness_values), 4)}")
+
+            average_fitness_values.append(average_fitness)
+
+            x_values = [individual[0] for individual in population[:10]]
+            y_values = [individual[1] for individual in population[:10]]
+            plot_population_values(generation, x_values, y_values)
+
+        if max(fitness_values) > best_overall_fitness:
+            best_overall_fitness = max(fitness_values)
+            best_overall_individual = population[np.argmax(fitness_values)]
+
+        parents = [roulette_selection(population, fitness_values) for _ in range(population_size)]
+
+        children = []
+        for i in range(0, population_size, 2):
+            parent1 = parents[i]
+            parent2 = parents[i + 1]
+            child1 = ordered_crossover(parent1, parent2)
+            child2 = ordered_crossover(parent2, parent1)
+            children.extend([mutation(child1, 0.25), mutation(child2, 0.25)])
+
+        population = children
+
+    print(f"\nOverall Best Solution: ({round(best_overall_individual[0], 4)}, {round(best_overall_individual[1], 4)}), Overall Best Fitness: {round(best_overall_fitness, 4)}")
+
+
+    plot_average_fitness(range(generations), average_fitness_values)
+    plot_3d_surface()
+
+genetic_algorithm(population_size=4, generations=100, populations_to_display=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
